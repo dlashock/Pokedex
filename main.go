1f46978325f3535f34347354e0a09100b5c3fb07
David Lashock Jr <dlashock@gmail.com>
2025-09-01 16:41:13 -0400

    Added 'help' and 'exit' command functionality.

--- main.go
+++ main.go
@@ -1,14 +1,68 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
+	"os"
 )
 
-func main() {
-	fmt.Println("Hello, World!")
-	words := cleanInput("My Name is pOOp butt . ")
-	for _, word := range words {
-		fmt.Println(word)
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+var commands map[string]cliCommand
+
+func commandExit() error {
+	fmt.Println("Closing the Pokedex... Goodbye!")
+	os.Exit(0)
+	return nil
+}
+
+func commandHelp() error {
+	fmt.Println("Welcome to the Pokedex!")
+	fmt.Println("Usage:")
+	fmt.Println()
+	for _, cmd := range commands {
+		fmt.Printf("%s: %s\n", cmd.name, cmd.description)
 	}
-	fmt.Println(len(words))
+	return nil
+}
+
+func main() {
+	commands = map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+	scanner := bufio.NewScanner(os.Stdin)
+	fmt.Print("Pokedex > ")
+	for scanner.Scan() {
+		line := scanner.Text()
+		words := cleanInput(line)
+		if len(words) == 0 {
+			fmt.Println("Please enter a command")
+			fmt.Print("Pokedex > ")
+			continue
+		}
+		command, exists := commands[words[0]]
+		if exists {
+			err := command.callback()
+			if err != nil {
+				fmt.Printf("An error has occurred: %s\n", err)
+			}
+		} else {
+			fmt.Println("Unknown command")
+		}
+		fmt.Print("Pokedex > ")
+	}
+
 }
